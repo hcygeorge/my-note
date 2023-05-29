bcdb38ea3c34910ccd54b762c5f89cac2dd2fc04
george <pch806771@ECONLINE.PRI>
2023-05-29 16:52:24 +0800

    add note

--- python/pandas_example02.py
+++ python/pandas_example02.py
@@ -11,10 +11,19 @@ df = pd.DataFrame(data)
 df
 
 #%% Question 1: Rearrange the table using the pivot function
-pivot_table = df.pivot(index='Name', columns='Region', values='Sales')
+pivot_table = df.pivot(
+    index='Name', columns='Region', values='Sales')
 print(pivot_table)
+print(pivot_table.index.name)
+
+#%%
+melted_table = pivot_table.reset_index().melt(
+    id_vars=['Name'], var_name=['Region'], value_name='Sales')
+print(melted_table)
+
+#%%
+df.pivot_table(index='Name', columns='Region', aggfunc=['sum', 'count'])
 
 #%% Question 2: Rearrange the table using the melt function
 melted_table = df.melt(id_vars=['Name', 'Region'], value_vars='Sales')
 print(melted_table)
-# %%
